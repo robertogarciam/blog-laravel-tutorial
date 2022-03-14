6e9520f6bb956f44180aa815727df674d90ec8e1
Roberto <robertovgarciam@hotmail.com>
2022-03-14 10:16:54 -0500

    Made Basic Page with posts and links

--- public/app.js
+++
@@ -1,1 +0,0 @@
-alert('Me Deh Yah');
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            lorem ipsum dolor sit amet Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum consectetur ipsa doloribus accusantium pariatur? Natus, aliquid? Fugiat ratione pariatur maiores doloribus mollitia, quam molestiae dolorum debitis esse, explicabo quo quasi!
+        </p>
+    </article>
+    
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            lorem ipsum dolor sit amet Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum consectetur ipsa doloribus accusantium pariatur? Natus, aliquid? Fugiat ratione pariatur maiores doloribus mollitia, quam molestiae dolorum debitis esse, explicabo quo quasi!
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            lorem ipsum dolor sit amet Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum consectetur ipsa doloribus accusantium pariatur? Natus, aliquid? Fugiat ratione pariatur maiores doloribus mollitia, quam molestiae dolorum debitis esse, explicabo quo quasi!
+        </p>
+    </article>
+
+</body>
+</html>
