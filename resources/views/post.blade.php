aa1164d029f665b58ba4e966e23e9a962daafef4
Roberto <robertovgarciam@hotmail.com>
2022-03-22 14:33:11 -0500

    Added layout opstion 2

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
     <article>        
-            <h1>{{$post->title}}</h1>
-            <div>
-                <!-- <?= $post->body ?> -->
-                {!!$post->body!!}
-            </div>
+        <h1>{{$post->title}}</h1>
+        <div>
+            <!-- <?= $post->body ?> -->
+            {!!$post->body!!}
+        </div>
     </article>
 
     <a href="/">Go Back</a>
-</body>
-</html>
+
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
     @foreach ($posts as $post)
         <article class="{{$loop->even ? 'foobar' : ''}}">
             <a href="/posts/{{$post->slug}}">
@@ -22,6 +13,5 @@
             </div>
         </article>
     @endforeach
+</x-layout>
 
-</body>
-</html>
